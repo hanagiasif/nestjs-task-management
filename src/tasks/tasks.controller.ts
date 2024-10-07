50ed02f1ddf08853bc1faf2852a5167d5f668c0a
hanagiasif <asifahmedhanagi01@gmail.com>
2024-10-07 15:11:25 +0530

    CRUD creast task completed

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
+
   @Get()
-  getAllTAsk() {
+  getAllTAsk(): Task[] {
     return this.taskService.getAllTask();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    return this.taskService.createTask(title, description);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Task, taskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class TasksService {
-  private tasks = [{ name: 'Asif ahmed hanagi' }];
+  private tasks: Task[] = [];
 
-  getAllTask() {
+  getAllTask(): Task[] {
     return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: taskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
   }
 }
